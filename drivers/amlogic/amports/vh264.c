ddbd217138d5d6d3788e22d72e318e458bb15064
WeTek <info@wetek.com>
2016-06-25 18:06:26 +0200

    disable h264 debug info spaming dmesg

--- drivers/amlogic/amports/vh264.c
+++ drivers/amlogic/amports/vh264.c
@@ -1793,10 +1793,11 @@ static void vh264_isr(void)
 			   ltemp); */
 			pr_info("0x%x\n", *daddr);
 		}
-#endif
+
 		pr_info("pocinfo 0x%x, top poc %d, wp 0x%x, length %d\n",
 			   READ_VREG(AV_SCRATCH_L), READ_VREG(AV_SCRATCH_M),
 			   sei_itu35_wp, sei_itu35_data_length);
+#endif
 		user_data_poc.poc_info = READ_VREG(AV_SCRATCH_L);
 		user_data_poc.poc_number = READ_VREG(AV_SCRATCH_M);
 		set_userdata_poc(user_data_poc);
